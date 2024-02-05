f13ea587212b39114d3d5caee67fee1daf662a9c
VanathiAmuthan <117298138+VanathiAmuthan@users.noreply.github.com>
2024-02-05 19:06:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ const CalcAge= document.getElementById("CalcAge");
 const displayAge= document.getElementById("displayAge");
 const Age= document.getElementById("age");
 var today = new Date();
-currDate.innerText=`Today's Date is : ${today.toLocaleDateString('en-IN')}`;
+currDate.innerText=`Today's Date is : ${today.toLocaleDateString('en-US')}`;
 
-CalcAge.addEventListener("click", () => {
-    let today = new Date();
-    let birthDate = new Date(dateOfBirth.value);
-    let age = today.getFullYear() - birthDate.getFullYear();
-    let m = today.getMonth() - birthDate.getMonth();
-
+CalcAge.addEventListener("click",()=>{
+    var birthDate = new Date(dateOfBirth.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age = age - 1;
     }
-
-    displayAge.style.visibility = "visible";
-    Age.innerText = `You are ${age} years old.`;
+displayAge.style.visibility="visible";
+Age.innerText=`You are ${age} years old.`
 });
